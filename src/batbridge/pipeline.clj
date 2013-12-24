087f775360ff262f75243db61b7aaf5e78d8cf7c
Reid McKenzie <rmckenzie92@gmail.com>
2013-12-24 16:58:42 -0600

    Rework the pipeline on the basis of the tested & stable single cycle

--- src/batbridge/pipeline.clj
+++ src/batbridge/pipeline.clj
@@ -1,237 +1,202 @@
 (ns batbridge.pipeline
-  "Implements a simple pipelined processor. Makes no attempt to
-  provide out-of-order or superscalar excutuion. Branch prediction and
-  hazard conditions are dealt with as minimally as possible."
-  (:require [clojure.core.typed :as t]
-            ; [clojure.tools.logging :refer [info]]
-            [batbridge.isa :as i]))
+  "Implements a simple pipelined processor. This processor makes no
+  effort to perform branch prediction, instruction caching or memory
+  caching. Correctness with respect to the pipeline and bubbles is
+  ensured however.")
 
+;; Instructions are represented as being length five vectors of the
+;; structure [icode dst param param lit]. Instructions are stored in
+;; state memory as one would expect.
 
-;; annotate clojure.core fns to behave as I intend
-;;------------------------------------------------------------------------------
-(t/ann ^:no-check clojure.core/assoc-in
-       (Fn [i/Processor '[i/Storage t/AnyInteger] t/AnyInteger -> i/Processor]))
+(defn seq->instrs [seq]
+  (zipmap (range 0 (* 4 (count seq)) 4)
+          seq))
 
+(defn instrs->state [instrs]
+  {:memory instrs
+   :registers {31 0}})
 
-(t/ann ^:no-check clojure.core/update-in
-       (Fn [i/Processor '[i/Storage t/AnyInteger]
-            (Fn [t/AnyInteger -> t/AnyInteger]) -> i/Processor]))
+;; The state structure will be represented as a map:
+;; {:memory {<int> <val>}
+;;  :registers {<int> <val>}
+;;  :halted <boolean>
+;; }
 
-(t/def-alias Meta
-  (HMap :mandatory {:fetch-pc t/AnyInteger
-                    :fetched? boolean}))
+;; In addition, the :fetch, :decode, :execute and :writeback keys will
+;; be used to store intermediate values used in computing the next
+;; state of the processor.
 
-(t/ann ^:no-check clojure.core/meta
-       (Fn [i/Instruction -> Meta]))
+(defn get-memory 
+  "Accesses an address in a processor state's memory, returning the
+  value there."
+  [p addr]
+  (get-in p [:memory addr] 0))
 
-(t/ann ^:no-check clojure.core/with-meta
-       (Fn [i/InstructionVec Meta -> i/InstructionVec]
-           [i/InstructionMap Meta -> i/InstructionMap]
-           [i/CommitCommand Meta -> i/CommitCommand]))
+(defn get-instr
+  "Accesses an address in a processor state's memory, returning the
+  value there. Provides a sane default for fetching instructions"
+  [p addr]
+  (get-in p [:memory addr] [:add 0 0 0 0]))
 
+(defn write-memory 
+  "Writes the argument value into the processor state's memory,
+  returning an updated processor state representation."
+  [p addr v]
+  (assoc-in p [:memory addr] v))
 
-;; holdover hack kept for niceties
-;;------------------------------------------------------------------------------
-(t/defn> ^:no-check get-memory
-  :- t/AnyInteger
-  [p    :- i/Processor
-   addr :- t/AnyInteger]
-  (get-in p [:memory addr]))
-
-(t/defn> ^:no-check get-icode
-  :- i/InstructionVec
-  [p    :- i/Processor
-   addr :- t/AnyInteger]
-  (get-in p [:memory addr]))
-
-(t/defn> ^:no-check get-register
-  :- t/AnyInteger
-  [p   :- i/Processor
-   reg :- t/AnyInteger]
+(defn get-register
+  "Fetches a register value by register ID from a processor state,
+  returning the value."
+  [p reg]
   (get-in p [:registers reg] 0))
 
+(defn halted? 
+  "Predicate to test whether a processor state record is halted."
+  [state]
+  (or (:halted state) false))
 
-;; processor implementation
-;;------------------------------------------------------------------------------
-(t/defn> fetch
-  "Implements the fetch stage"
-  :- i/Processor
-  [processor :- i/Processor]
-  (let [pc (get-register processor 31)
-        icode (or (get-icode processor pc) i/vec-no-op)]
-    (println (format "[fetch    ] pulling from address %d : %s" pc icode))
-    (-> processor
-        (assoc-in [:registers 31] (+ pc 4))
-        (assoc :fetch (with-meta icode
-                        {:fetch-pc pc
-                         :fetched? true})))))
-
-
-(t/defn> ^:no-check in-flight
-  :- (t/Set i/InstructionParam)
-  [processor :- i/Processor]
-  (set
-   [(get-in processor [:decode :dst])
-    (when (= :registers (get-in processor [:execute 0]))
-      (get-in processor [:execute 1]))]))
-
-
-(t/defn> decode
-  "Dummy decode which translates a vector literal to an icode map. Not really
-  important now, but it'll be nice later."
-  :- i/Processor
-  [processor :- i/Processor]
-  (let [icode (get processor :fetch i/vec-no-op)
-        decoded {:icode (nth icode 0 :add)
-                 :dst   (get i/register-symbol-map (nth icode 1 0) 0)
-                 :srca  (get i/register-symbol-map (nth icode 2 0) 0)
-                 :srcb  (get i/register-symbol-map (nth icode 3 0) 0)
-                 :lit   (nth icode 4 0)}
-        decoded (with-meta decoded (meta icode))]
-    (if (and (i/fetched? icode)
-             (and (i/fetched? (:decode processor))
-                  (or (contains? (in-flight processor) (:srca decoded))
-                      (contains? (in-flight processor) (:srcb decoded)))))
-      (do (println (format "[decode   ] data hazard detected! setting pc to %d..."
-                           (:fetch-pc (meta icode))))
-          (-> processor
-              (assoc :decode i/map-no-op)
-              (assoc-in [:registers 31] (:fetch-pc (meta icode)))))
-      (do (println "[decode   ] no data hazard.. decoding.. done.")
-          (assoc processor :decode decoded)))))
-
-
-(t/defn> register->val
-  :- t/AnyInteger
-  [reg       :- i/InstructionParam
-   processor :- i/Processor]
+(defn register->val 
+  "Helper function to compute a value from either a keyword register
+  alias or an integer register identifier. Returnes the value of
+  accessing the identified register."
+  [reg processor]
   (case reg
     (:r_ZERO 0) 0
     (:r_IMM 30) (get (get processor :decode {}) :lit 0)
     (:r_PC 31)  (get-register processor 31)
-    (-> processor
+    (-> processor 
         :registers
         (get reg 0))))
 
+(def register-symbol-map
+  "Maps register abbreviations and IDs to register IDs."
+  (-> (zipmap (range 30) (range 30))
+      (assoc :r_PC 31)
+      (assoc :r_IMM 30)
+      (assoc :r_ZERO 0)))
 
-(t/ann ^:no-check opcode->fn
-       (t/Map t/AnyInteger
-              (Fn [t/AnyInteger t/AnyInteger i/Processor
-                   -> t/AnyInteger])))
 (def opcode->fn
-  {:add (fn [x y _] (+ x y))
-   :sub (fn [x y _] (- x y))
+  "Maps opcode names to implementing functions."
+  {:add  (fn [x y _ dst] [:registers dst (+ x y)])
+   :sub  (fn [x y _ dst] [:registers dst (- x y)])
+   :mul  (fn [x y _ dst] [:registers dst (* x y)])
+   :div  (fn [x y _ dst] [:registers dst (/ x y)])
+   :mod  (fn [x y _ dst] [:registers dst (mod x y)])
+   :shr  (fn [x y _ dst] [:registers dst (bit-shift-right x y)])
+   :shl  (fn [x y _ dst] [:registers dst (bit-shift-left x y)])
+   :ld   (fn [x y p dst] [:registers dst (get-memory p (+ x (* 4 y)))])
+   :st   (fn [x y p dst] [:memory (+ x (* 4 y)) (get-register p dst)])
+   :halt (fn [_ _ _ _]   :halt)
    })
 
 
-(t/defn> execute
-  :- i/Processor
-  [processor :- i/Processor]
-  (let [icode (get processor :decode i/map-no-op)
+(defn fetch 
+  "Accesses the value of the PC register, fetches the instruction
+  there from memory and sets the :fetch value key in the
+  state. Increments the PC by the instruction width. Returns an
+  updated state."
+  [processor]
+  (let [pc (get-register processor 31)
+        icode (get-instr processor pc)]
+    (assert (vector? icode))
+    (println "[fetch    ]" pc "->" icode)
+    (-> processor
+        (update-in [:registers 31] (fn [x] (+ x 4)))
+        (assoc :fetch icode))))
+
+
+(defn decode
+  "Dummy decode which translates a vector literal to an icode map. Not
+  really important now because there is no binary decoding to do, but
+  it'll be nice later."  
+  [processor]
+  (let [icode (get processor :fetch [:add 0 0 0 0])]
+    (println "[decode   ]" icode)
+    (assoc processor :decode
+           {:icode (nth icode 0 :add)
+            :dst   (get register-symbol-map (nth icode 1 0) 0)
+            :srca  (get register-symbol-map (nth icode 2 0) 0)
+            :srcb  (get register-symbol-map (nth icode 3 0) 0)
+            :lit   (nth icode 4 0)})))
+
+
+(defn execute 
+  "Indexes into the opcode->fn map to fetch the implementation of the
+  last decoded instruction. Decodes the parameter values, and applies
+  the implementation function to the parameters. Sets the :execute key
+  with a state update directive which can use. Returns the updated
+  processor state."
+  [processor]
+  (let [icode (:decode processor)
         srca  (register->val (get icode :srca 0) processor)
         srcb  (register->val (get icode :srcb 0) processor)
-        dst   (get i/register-symbol-map (get icode :dst 0) 0)
-        opcode (get icode :icode :add)]
-
-    (when-not (i/fetched? icode)
-      (println "[execute  ] processing no-op..."))
-
-    (as-> (case opcode
-            (:add :sub)
-              (do (println "[execute  ] processing registered op...")
-                  [:registers dst ((get opcode->fn opcode (constantly 0))
-                                   srca srcb processor)])
-
-            (:ld)
-              (do (println "[execute  ] processing load op...")
-                  [:load (+ srca (* 4 srcb)) dst])
-
-            (:st)
-              (do (println "[execute  ] processing store op...")
-                  [:store (+ srca (* 4 srcb)) dst])
-
-            (do (println "[execute  ] sending halt...") [:halt]))
-          cmd
-        (with-meta cmd (meta icode))
-        (assoc processor :execute cmd))))
+        dst   (get icode :dst 0)
+        icode (get icode :icode :add)]
+    (println "[execute  ]" (:decode processor))
+    (as-> icode v
+          (get opcode->fn v (constantly :halt))
+          (v srca srcb processor dst)
+          (assoc processor :execute v))))
 
 
-(t/defn> writeback
-  :- i/Processor
-  [processor :- i/Processor]
-  (let [directive (get processor :execute
-                       (with-meta
-                         [:registers 0 0]
-                         {:fetched? false
-                          :fetch-pc -1}))]
-    (when-not (i/fetched? directive)
-      (println "[writeback] processing no-op..."))
-
+(defn writeback 
+  "Pulls a writeback directive out of the processor state, and
+  performs the indicated update on the processor state. Updates are
+  either the value :halt, or vectors [:memory <addr> <val>] or
+  [:registers <addr> <val>]. Returns an updated state."
+  [processor]
+  (let [directive (get processor :execute [:registers 0 0])]
+    (println "[writeback]" directive)
     (cond ;; special case to stop the show
-          (= :halt (first directive))
-            (do (println "[writeback] halting...")
-                (assoc processor :halted true))
-
+          (= :halt directive)
+            (assoc processor :halted true)
+          
           ;; special case for printing
           (and (= :registers (first directive))
                (= 0 (second directive)))
-            (if-not (zero? (nth directive 2 0))
-              (do (print (char (nth directive 2 0)))
-                  processor)
-              processor)
+          (do (when-not (zero? (nth directive 2 0))
+                (print (char (nth directive 2 0))))
+                processor)
 
-          ;; ;; special case for setting the PC
-          ;; ;;
-          ;; ;; The pipeline has to be flushed in this implementation
-          ;; ;; because we don't know (and can't know) if the in-flight
-          ;; ;; instructions are on the right execution path.
-          ;; (and (= :registers (first directive))
-          ;;      (= 31 (second directive)))
-          ;;   (do (println "[writeback] Setting PC and flushing the pipeline...")
-          ;;     (-> processor
-          ;;         (assoc-in [:registers 31] (second directive))
-          ;;         (dissoc :fetch :decode :execute :writeback)))
-
-          (= (nth directive 0) :registers)
-            (do (println "[writeback] processing register store...")
-              (assoc-in processor [:registers (nth directive 1 0)]
-                        (nth directive 2 0)))
+          ;; special case for branching as we must flush the pipeline
+          (and (= :registers (first directive))
+               (= 31 (second directive)))
+            (do (println "[writeback] flushing pipeline!")
+                (-> processor
+                    (dissoc :fetch)
+                    (dissoc :decode)
+                    (dissoc :execute)
+                    (assoc-in [(nth directive 0 :registers) (nth directive 1 0)]
+                              (nth directive 2 0))))
 
           true
-            (do (println (format "[writeback] ERROR: UNMATCHED COMMAND\n\t%s"
-                                 directive))
-                processor))))
+          (assoc-in processor [(nth directive 0 :registers)
+                               (nth directive 1 0)]
+                 (nth directive 2 0)))))
 
 
-(t/defn> step
+(defn step
   "Sequentially applies each phase of execution to a single processor
   state, returning an updated state representing a single cycle of
-  computation's effects on the processor."
-  :- i/Processor
-  [state :- i/Processor]
+  computation's effects on the processor. Because this is a pipelined
+  design, we simply take the in-order steps and run them in the
+  reverse order using the processor state as a set of latches for
+  storing the state between clock 'cycles'."
+  [state]
   (-> state
-      (writeback)
-      (execute)
-      (decode)
-      (fetch)))
+      writeback
+      execute
+      decode
+      fetch))
 
-(t/ann ^:no-check run [i/Processor -> i/Processor])
-(defn run [state & {:keys [limit]
-                    :as kwargs
-                    :or {limit 50}}]
-  (loop [state state
-         i limit]
-    (if-not (or (i/halted? state)
-                (zero? limit))
-      (let [ns (step state)]
-        (println "---------------------------------------------------")
-        (recur ns (dec i)))
-      state)))
 
-(t/ann ^:no-check -main [(t/Map t/AnyInteger i/InstructionVec) -> i/Processor])
 (defn -main
-  "Creates a processor state with a map \"instructions\" from
-  4-aligned addresses to instruction representations, and executes
-  this state starting from memory address 0."
-  [instructions]
-  (run (i/seq->state instructions)))
+  "Steps a processor state until such time as it becomes marked as
+  'halted'. Makes no attempt to construct or validate a processor
+  state, or handle errors therein." 
+  [state]
+  (loop [state state]
+    (if-not (halted? state)
+      (do (println "-------------------------------------------------")
+          (recur (step state)))
+      state)))
