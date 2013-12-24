5142574c5ae1feaea2a4fa7f0ce04ff6b3f49f46
Reid McKenzie <rmckenzie92@gmail.com>
2013-12-24 16:16:49 -0600

    Refactor & test the entire processor
     - Rewrite the execute operation to be entirely modular
     - Clean up -main to do no implicit wrapping
     - Test the load/store instructions
     - Add a better implicit halt

--- src/batbridge/single_cycle.clj
+++ src/batbridge/single_cycle.clj
@@ -2,18 +2,28 @@
   "Implements a simple in-order processor. Makes no attempt to provide
   out-of-order or superscalar excutuion. Will serve as a benchmark for per-cycle
   operation performance and to allow correctness comparison between multiple
-  differing processor implementations."
-  (:require [clojure.tools.logging :refer [error info]]))
+  differing processor implementations.")
+
+(def register-symbol-map
+  (-> (zipmap (range 30) (range 30))
+      (assoc :r_PC 31)
+      (assoc :r_IMM 30)
+      (assoc :r_ZERO 0)))
 
 (defn get-memory [p addr]
   (get-in p [:memory addr] 0))
 
+(defn write-memory [p addr v]
+  (assoc-in p [:memory addr] v))
+
 (defn get-register [p reg]
-  (get-in p [:register reg] 0))
+  (get-in p [:registers reg] 0))
 
 (defn fetch [processor]
   (let [pc (get-register processor 31)
         icode (get-memory processor pc)]
+    (assert (vector? icode))
+    (println "[fetch]" pc icode)
     (-> processor
         (update-in [:registers 31] (fn [x] (+ x 4)))
         (assoc :fetch icode))))
@@ -25,9 +35,9 @@
   (let [icode (:fetch processor)]
     (assoc processor :decode
            {:icode (nth icode 0 :add)
-            :dst   (get i/register-symbol-map (nth icode 1 0) 0)
-            :srca  (get i/register-symbol-map (nth icode 2 0) 0)
-            :srcb  (get i/register-symbol-map (nth icode 3 0) 0)
+            :dst   (get register-symbol-map (nth icode 1 0) 0)
+            :srca  (get register-symbol-map (nth icode 2 0) 0)
+            :srcb  (get register-symbol-map (nth icode 3 0) 0)
             :lit   (nth icode 4 0)})))
 
 (defn register->val [reg processor]
@@ -40,28 +50,28 @@
         (get reg 0))))
 
 (def opcode->fn
-  {:add (fn [x y _] (+ x y))
-   :sub (fn [x y _] (- x y))
-   :ld  (fn [x y p] (get-memory p (+ x (* 4 y))))
+  {:add  (fn [x y _ dst] [:registers dst (+ x y)])
+   :sub  (fn [x y _ dst] [:registers dst (- x y)])
+   :mul  (fn [x y _ dst] [:registers dst (* x y)])
+   :div  (fn [x y _ dst] [:registers dst (/ x y)])
+   :mod  (fn [x y _ dst] [:registers dst (mod x y)])
+   :shr  (fn [x y _ dst] [:registers dst (bit-shift-right x y)])
+   :shl  (fn [x y _ dst] [:registers dst (bit-shift-left x y)])
+   :ld   (fn [x y p dst] [:registers dst (get-memory p (+ x (* 4 y)))])
+   :st   (fn [x y p dst] [:memory (+ x (* 4 y)) (get-register p dst)])
+   :halt (fn [_ _ _ _]   :halt)
    })
 
 (defn execute [processor]
   (let [icode (:decode processor)
         srca  (register->val (get icode :srca 0) processor)
         srcb  (register->val (get icode :srcb 0) processor)
-        dst   (get i/register-symbol-map (get icode :dst 0) 0)
+        dst   (get register-symbol-map (get icode :dst 0) 0)
         icode (get icode :icode :add)]
-    (assoc processor :execute
-           (case icode
-             (:add :sub :ld)
-               [:registers dst ((get opcode->fn icode (constantly 0))
-                                      srca srcb processor)]
-
-             (:st)
-               [:memory (+ srca (* 4 srcb)) dst]
-
-             :else
-               :halt))))
+    (as-> icode v
+          (get opcode->fn v (constantly :halt))
+          (v srca srcb processor dst)
+          (assoc processor :execute v))))
 
 (defn writeback [processor]
   (let [directive (:execute processor)]
@@ -94,21 +104,24 @@
       (execute)
       (writeback)))
 
-(defn -main
-  "Creates a processor state with a map \"instructions\" from
-  4-aligned addresses to instruction representations, and executes
-  this state starting from memory address 0."
-  [instructions]
-  (loop [state {:memory instructions
-                :registers {31 0}}]
-    (if-not (halted? state)
-      (recur (step state))
-      state)))
-
 (defn seq->instrs [seq]
   (zipmap (range 0 (* 4 (count seq)) 4)
           seq))
 
+(defn instrs->state [instrs]
+  {:memory instrs
+   :registers {31 0}})
+
 (defn bprn [str]
-  (map (fn [x :- Character] [:add :r_ZERO :r_IMM 0 (int x)])
+  (map (fn [x] [:add :r_ZERO :r_IMM 0 (int x)])
        str))
+
+(defn -main
+  "Steps a processor state until such time as it becomes marked as
+  'halted'. Makes no attempt to construct or validate a processor
+  state, or handle errors therein." 
+  [state]
+  (loop [state state]
+    (if-not (halted? state)
+      (recur (step state))
+      state)))
