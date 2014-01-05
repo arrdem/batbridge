c915376de274fe1df799d5564e41f1959535f39b
Reid McKenzie <rmckenzie92@gmail.com>
2014-01-05 00:44:26 -0600

    Implement pipeline stalls
    I forgot about data hazards in pipelines.. This diff implements at least
    near correct support for data hazards. Note that special cases are
    provided so that hazards against the immediate and zero register are
    ignored because writing to those registers is meaningless in terms of
    processor internal state.

--- src/batbridge/pipeline.clj
+++ src/batbridge/pipeline.clj
@@ -5,10 +5,80 @@
   ensured however."
   (:require [batbridge [isa :as isa]
                        [common :as common]
-                       [single-cycle :as ss]]))
+                       [single-cycle :as ss]]
+            [clojure.set :as set]))
 
 
-(defn writeback 
+;; There is an extra issue in the pipelined processor, one which I
+;; completely forgot about until misbehavior in my test suite exposed
+;; it from its lurkings.
+;; 
+;; When it takes nonzero time for the "previous" instruction to
+;; register changes in memory or in registers, it is very possible
+;; that if the "subsequent" instruction depends on the change to be
+;; made by the previous instruction one cannot simply pipeline all the
+;; instructions together. Data changes made by previous instructions
+;; must _if relevant_ be visible to subsequent instructions.
+;;
+;; We already have pipeline flushing on branching, now we need
+;; pipeline stalling on dependency conflicts. That is, if the source
+;; of the following instruction is the destination of the instruction
+;; in decode, the instruction in decode must be delayed so that when
+;; it executes the preceding change will be visible.
+;;
+;; In order to provide this behavior I will add a :stall key to the
+;; pipelined processor's state. Every cycle, the :stall key will be
+;; decremented _by step_, bounded at zero. If the stall key is
+;; nonzero, then Fetch and Decode become the identity function. That
+;; is they do no work until the value in Decode becomes
+;; meaningful. The exception to this rule is a pipeline flush, which
+;; zeros the stall counter and clears all stages.
+
+
+(defn stalled?
+  "Checks the stall counter, returning True if the stall counter is
+  nonzero. A nil value is treated as zero."
+
+  [processor]
+  (-> processor
+      (get :stall 0)
+      zero? not))
+
+
+(defn fetch
+  "Checks the stall counter, invoking ss/fetch if zero otherwise
+  returning the input state unmodified due to a pipeline stall."
+
+  [processor]
+  (if (stalled? processor) 
+    processor
+    (ss/fetch processor)))
+
+
+(defn decode
+  "Checks the stall counter, invoking ss/decode and checking for a
+  pipeline hazzard in the result if zero otherwise returning the input
+  state unmodified due to a pipeline stall."
+
+  [processor]
+  (if (stalled? processor) 
+    processor
+    (let [next-processor (ss/decode processor)
+          {:keys [decode execute]} next-processor
+          {:keys [dst addr]} execute]
+      (if (and (= :registers dst)
+               (contains? (-> #{}
+                              (into [(:a decode) (:b decode)])
+                              (disj 30 29))
+                          addr))
+        (do (println "[decode   ] stalling the pipeline!")
+            (-> processor 
+                (assoc :stall 1)
+                (dissoc :decode)))
+        next-processor))))
+
+
+(defn writeback
   "Pulls a writeback directive out of the processor state, and
   performs the indicated update on the processor state. Update command
   have been restructured and are now maps
@@ -50,6 +120,15 @@
             (assoc-in processor [dst addr] val))))
 
 
+(defn stall-dec
+  "A dec which deals with a nil argument case, and has a floor value
+  of 0."
+
+  [nillable-value]
+  (let [v (or nillable-value 0)]
+    (max 0 (dec v))))
+
+
 (defn step
   "Sequentially applies each phase of execution to a single processor
   state, returning an updated state representing a single cycle of
@@ -59,11 +138,13 @@
   storing the state between clock 'cycles'."
 
   [state]
-  (-> state
-      writeback
-      ss/execute
-      ss/decode
-      ss/fetch))
+  (do (println "-------------------------------------------------")
+      (-> state
+          writeback
+          ss/execute
+          decode
+          fetch
+          (update-in [:stall] stall-dec))))
 
 
 (defn -main
