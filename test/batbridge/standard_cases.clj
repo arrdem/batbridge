9a8f58a7f68bdc095ea149da920e57d1a6d11664
Reid McKenzie <rmckenzie92@gmail.com>
2014-01-05 01:44:24 -0600

    Use test/is rather than booleans
    Clean up the test case predicates to use clojure.test/is rather than
    assert or combinational logic. This allows for the specific failure to
    be reported to the programmer rather than a simple indication of the
    failure's existence.
    
    Also mixed in here is a whitespace "cleanup" and a rewrite of the
    run-test fn to use the new t/ alias for clojure.test/

--- test/batbridge/standard_cases.clj
+++ test/batbridge/standard_cases.clj
@@ -8,7 +8,7 @@
                        [bytecode  :as b]]
             [toothpick.assembler :refer [assemble]]
             [toothpick.isa.batbridge :refer [batbridge]]
-            clojure.test))
+            [clojure.test :as t]))
 
 
 (defrecord ps-test [opcodes predicate bound])
@@ -18,26 +18,26 @@
   `(def ~sym (->ps-test ~opcodes ~predicate ~bound)))
 
 
-(defn run-test 
+(defn run-test
   "Destructures and runs a given test record using the specified step
   function. This makes run-test parametric on what simulator is to be
   used in running the unstructions. Uses clojure.test/is for assertion
   checking."
-  
+
   [test step]
   (let [{:keys [opcodes predicate bound]} test
         state (c/instrs->state opcodes)]
     ;; (println "[]" opcodes)
     ;; (println "[]" predicate)
     ;; (println "[]" bound)
-    (clojure.test/is
-     (predicate
-      (loop [state state
-             bound bound]
-        (if (or (c/halted? state)
-                (= bound 0))
-          state
-          (recur (step state) (dec bound))))))))
+    (t/is (predicate
+           (loop [state state
+                  bound bound]
+             (if (or (c/halted? state)
+                     (= bound 0))
+               state
+               (recur (step state)
+                      (dec bound))))))))
 
 
 ;;------------------------------------------------------------------------------
@@ -50,25 +50,27 @@
   ;; r3 is used to count down from 15 for loop control
 
   {;; load constants into registers
-   0   [:add  0 :r_ZERO :r_IMM 0]         ;; preload fib(0)
-   4   [:add  1 :r_ZERO :r_IMM 1]         ;; preload fib(1)
-   8   [:add  3 :r_ZERO :r_IMM 14]        ;; the loop bound
+   0   [:add  0 :r_ZERO :r_IMM 0]      ;; preload fib(0)
+   4   [:add  1 :r_ZERO :r_IMM 1]      ;; preload fib(1)
+   8   [:add  3 :r_ZERO :r_IMM 14]     ;; the loop bound
 
    ;; write the fib loop
-   12  [:add  2 1 0 0]                    ;; add fib(n-2) + fib(n-1), store to fib
-   16  [:add  0 1 :r_ZERO 0]              ;; move fib(n-1) down
-   20  [:add  1 2 :r_ZERO 0]              ;; move fib(n) down
-   24  [:sub  3 3 :r_IMM  1]              ;; dec the loop constant
-   28  [:ifne 0 3 :r_ZERO 0]              ;; test if we've zeroed the loop counter yet
-   32  [:add  :r_PC :r_ZERO :r_IMM 12]    ;; if not jump to the top of the loop
-   36  [:halt 0 0 0 0]                    ;; otherwise halt
+   12  [:add  2 1 0 0]                 ;; add fib(n-2) + fib(n-1), store to fib
+   16  [:add  0 1 :r_ZERO 0]           ;; move fib(n-1) down
+   20  [:add  1 2 :r_ZERO 0]           ;; move fib(n) down
+   24  [:sub  3 3 :r_IMM  1]           ;; dec the loop constant
+   28  [:ifne 0 3 :r_ZERO 0]           ;; test if we've zeroed the loop counter yet
+   32  [:add  :r_PC :r_ZERO :r_IMM 12] ;; if not jump to the top of the loop
+   36  [:halt 0 0 0 0]                 ;; otherwise halt
    }
-   
+
   (fn [state]
-    (and (= (c/get-register state 2) 610) ;; fib 15 with th (0,1) as the base case
-         (c/halted? state)))              ;; the processor should have halted correctly
- 
-  100)
+    (t/is (= (c/get-register state 2)  ;; fib 15 with th (0,1) as the base case
+             610))
+    (t/is (c/halted? state))           ;; the processor should have halted correctly
+    true)
+
+  1000)
 
 
 (deftest fact-test
@@ -77,21 +79,21 @@
   ;; This is essentially the same as (reduce * (range 1 11)).
 
   {;; load constants into registers
-   0   [:add  0 :r_ZERO :r_IMM 1]         ;; preload fact(0)
-   4   [:add  1 :r_ZERO :r_IMM 10]        ;; the loop bound
+   0   [:add  0 :r_ZERO :r_IMM 1]     ;; preload fact(0)
+   4   [:add  1 :r_ZERO :r_IMM 10]    ;; the loop bound
 
    ;; write the fact loop
-   8  [:mul  0 0 1 0]
-   12  [:sub  1 1 :r_IMM  1]              ;; dec the loop constant
-   16  [:ifne 0 1 :r_ZERO 1]              ;; test if we've zeroed the loop counter yet
-   20  [:add  :r_PC :r_ZERO :r_IMM 8]     ;; if not jump to the top of the loop
-   24  [:halt 0 0 0 0]                    ;; otherwise halt
+   8   [:mul  0 0 1 0]
+   12  [:sub  1 1 :r_IMM  1]          ;; dec the loop constant
+   16  [:ifne 0 1 :r_ZERO 0]          ;; test if we've zeroed the loop counter yet
+   20  [:add  :r_PC :r_ZERO :r_IMM 8] ;; if not jump to the top of the loop
+   24  [:halt 0 0 0 0]                ;; otherwise halt
    }
-   
+
   (fn [state]
-    (and (= (c/get-register state 0)
-            3628800)                      ;; fact(10)
-         (c/halted? state)))              ;; the processor should have halted correctly
- 
-  100)
-  
+    (t/is (= (c/get-register state 0)
+             3628800))                ;; fact(10)
+    (t/is (c/halted? state)   )       ;; the processor should have halted correctly
+    true)
+
+  1000)
