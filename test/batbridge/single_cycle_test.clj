f30dc7785d10276ac0417e487b0cc30d750b810a
Reid McKenzie <rmckenzie92@gmail.com>
2014-01-04 23:53:37 -0600

    Test the single cycle over the standard tests
    Add deftests to run the single cycle processor against the standard test
    suite.

--- test/batbridge/single_cycle_test.clj
+++ test/batbridge/single_cycle_test.clj
@@ -1,3 +1,14 @@
 (ns batbridge.single-cycle-test
   (:require [batbridge.single-cycle :as ss]
+            [batbridge.standard-cases :as sc]
             [clojure.test :refer :all]))
+
+
+(deftest fib-test
+  ;; runs the fib test on the single cycle machine
+  (sc/run-test sc/fib-test ss/step))
+
+
+(deftest fact-test
+  ;; runs the factorial test on the single cycle machine
+  (sc/run-test sc/fact-test ss/step))
