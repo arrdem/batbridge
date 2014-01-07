7083623a610004406448d28200f543b28cce75b9
Reid McKenzie <rmckenzie92@gmail.com>
2014-01-07 17:04:43 -0600

    Add the bytecode tests to the single cycle test suite

--- test/batbridge/single_cycle_test.clj
+++ test/batbridge/single_cycle_test.clj
@@ -9,9 +9,10 @@
 
 (deftest fib-test
   ;; runs the fib test on the single cycle machine
-  (sc/run-test sc/fib-test ss/step bound))
-
+  (sc/run-test sc/fib-test ss/step bound)
+  (sc/run-test sc/fib-byte-test ss/step bound))
 
 (deftest fact-test
   ;; runs the factorial test on the single cycle machine
-  (sc/run-test sc/fact-test ss/step bound))
+  (sc/run-test sc/fact-test ss/step bound)
+  (sc/run-test sc/fact-byte-test ss/step bound))
