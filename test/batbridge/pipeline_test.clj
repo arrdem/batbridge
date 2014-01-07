8a79d1a68e03e7c75c0770cef244e3dfa289f3f9
Reid McKenzie <rmckenzie92@gmail.com>
2014-01-07 17:05:32 -0600

    Make the byte encoded tests part of the pipeline suite

--- test/batbridge/pipeline_test.clj
+++ test/batbridge/pipeline_test.clj
@@ -9,9 +9,11 @@
 
 (deftest fib-test
   ;; runs the fib test on the single cycle machine
-  (sc/run-test sc/fib-test p/step bound))
+  (sc/run-test sc/fib-test p/step bound)
+  (sc/run-test sc/fib-byte-test p/step bound))
 
 
 (deftest fact-test
   ;; runs the factorial test on the single cycle machine
-  (sc/run-test sc/fact-test p/step bound))
+  (sc/run-test sc/fact-test p/step bound)
+  (sc/run-test sc/fact-byte-test p/step bound))
