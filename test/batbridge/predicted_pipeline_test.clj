dc3ce85e8119fcd71515fc68d4c108d3153e2f00
arrdem <rmckenzie92@gmail.com>
2014-03-27 21:49:04 -0500

    Add standard tests for the predicted processor
    This commit extends the test suite to run the normal tests over the
    branch predicted, pipelined processor implementation.

---
+++ test/batbridge/predicted_pipeline_test.clj
@@ -0,0 +1,18 @@
+(ns batbridge.predicted-pipeline-test
+  (:require [batbridge.predicted-pipeline :as pp]
+            [batbridge.standard-cases :as sc]
+            [clojure.test :refer :all]))
+
+
+(def bound 100)
+
+
+(deftest fib-test
+  ;; runs the fib test on the single cycle machine
+  (sc/run-test sc/fib-test pp/step bound)
+  (sc/run-test sc/fib-byte-test pp/step bound))
+
+(deftest fact-test
+  ;; runs the factorial test on the single cycle machine
+  (sc/run-test sc/fact-test pp/step bound)
+  (sc/run-test sc/fact-byte-test pp/step bound))
