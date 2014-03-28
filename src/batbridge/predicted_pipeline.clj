3de5bd35daed66ca559e2526aea445341c6bd053
arrdem <rmckenzie92@gmail.com>
2014-03-27 22:28:15 -0500

    Whitespace cleanup

--- src/batbridge/predicted_pipeline.clj
+++ src/batbridge/predicted_pipeline.clj
@@ -7,16 +7,16 @@
                        [pipeline :as p]
                        [common :as common]]
             [taoensso.timbre :refer [info warn]]
-            [amalloy.ring-buffer 
+            [amalloy.ring-buffer
              :refer [ring-buffer]]))
 
 ;; Build a simple branch predictor typeclass.
 ;; Several predictors are implemented as from McFarling[1]
-;; 
+;;
 ;; By convention, I will refer to the "taken" state if an instruction
 ;; branches to any address besides PC+4. "branching" to PC+4 is
 ;; considered to be not taken.
-;; 
+;;
 ;; ------------------------------------------------------------------------------
 ;; Here I will implement GShare as from [1]
 
@@ -52,17 +52,17 @@
 (defn taken?
   "Examines a processor state, determining whether the state of the
   Writeback stage qualifies under my definition of taken vs. not
-  taken." 
+  taken."
 
   [processor]
   (let [{:keys [dst addr val pc]} (:execute processor)]
-    (cond (or (and (= dst :registers) 
-                   (= addr 31) 
+    (cond (or (and (= dst :registers)
+                   (= addr 31)
                    (= val pc))
               (not (= addr 31)))
             :not-taken
 
-          (and (= dst :registers) 
+          (and (= dst :registers)
                (= addr 31))
             :taken)))
 
@@ -71,7 +71,7 @@
   "Examines a processor state, determining the next value for the
   PC. Note that this relies on support from writeback to record PC
   value transitions when jumps are executed."
-  
+
   [processor]
   (let [{:keys [jump-map pred hst]} (:predictor processor)
         pc                          (common/get-register processor 31)]
@@ -129,7 +129,7 @@
   returning the input state unmodified due to a pipeline stall."
 
   [processor]
-  (if (p/stalled? processor) 
+  (if (p/stalled? processor)
     processor
     (let [pc    (common/get-register processor 31)
           icode (common/get-memory processor pc)
@@ -137,7 +137,7 @@
       (info "[fetch    ]" pc "->" icode " npc:" npc)
       (-> processor
           (assoc-in [:registers 31] npc)
-          (assoc :fetch {:icode icode 
+          (assoc :fetch {:icode icode
                          :npc   npc
                          :pc    (+ pc 4)})))))
 
@@ -146,7 +146,7 @@
   "Pulls a writeback directive out of the processor state, and
   performs the indicated update on the processor state. Update command
   have been restructured and are now maps
-  {:dst (U :registers :halt :memory) :addr Int :val Int}."  
+  {:dst (U :registers :halt :memory) :addr Int :val Int}."
 
   [processor]
   (let [directive (get processor :execute [:registers 30 0])
@@ -155,7 +155,7 @@
     (cond ;; special case to stop the show
           (= :halt dst)
             (assoc processor :halted true)
-          
+
           ;; special case for hex code printing
           (and (= :registers dst)
                (= 29 addr))
@@ -169,7 +169,7 @@
             (do (when-not (zero? val)
                   (print (char val)))
                 processor)
-            
+
           ;; special case for branching as we must flush the pipeline
           (and (= :registers dst)
                (= 31 addr)
@@ -221,7 +221,7 @@
 (defn -main
   "Steps a processor state until such time as it becomes marked as
   'halted'. Makes no attempt to construct or validate a processor
-  state, or handle errors therein." 
+  state, or handle errors therein."
 
   [state]
   (loop [state state]
