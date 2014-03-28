2913e660bee2f786360ddab3585b9521cd9e5be0
arrdem <rmckenzie92@gmail.com>
2014-03-27 21:58:35 -0500

    Add amalloy's ring-buffer as a dep

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [me.arrdem/toothpick "0.1.1"]])
+                 [me.arrdem/toothpick "0.1.1"]
+                 [amalloy/ring-buffer "1.0"]])
