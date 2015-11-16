b3da3b4b7c7725b8609419e7af5b190ef4d8e9a6
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-11-16 15:28:38 -0600

    Whitespace

--- src/batbridge/predicted_pipeline.clj
+++ src/batbridge/predicted_pipeline.clj
@@ -3,12 +3,12 @@
   effort to perform branch prediction, instruction caching or memory
   caching. Correctness with respect to the pipeline and bubbles is
   ensured however."
-  (:require [batbridge [single-cycle :as ss]
-                       [pipeline :as p]
-                       [common :as common]]
+  (:require [batbridge
+             [single-cycle :as ss]
+             [pipeline :as p]
+             [common :as common]]
             [taoensso.timbre :refer [info warn]]
-            [amalloy.ring-buffer
-             :refer [ring-buffer]]))
+            [amalloy.ring-buffer :refer [ring-buffer]]))
 
 ;; Build a simple branch predictor typeclass.
 ;; Several predictors are implemented as from McFarling[1]
@@ -25,15 +25,12 @@
     (:inc) (min (inc (or v 2)) 3)
     (:dec) (max (dec (or v 2)) 0)))
 
-
 (defn bool->int [bool]
   (if bool 1 0))
-
 
 (defn vec->bitv [bool-vec]
   (reduce (fn [c b] (bit-or (bit-shift-left c 1) (bool->int b)))
           0 bool-vec))
-
 
 (defn train-pred [p addr hst res]
   (let [a [(bit-xor (bit-and 0x1FF addr)
@@ -41,7 +38,6 @@
     (case res
       (:taken)     (update-in p a two-bit-counter :inc)
       (:not-taken) (update-in p a two-bit-counter :dec))))
-
 
 (defn predict-pred [p addr hst]
   (>= (get p (bit-xor addr (vec->bitv hst)) 2) 2))
@@ -62,7 +58,6 @@
       (get jump-map pc)
       (+ 4 pc))))
 
-
 ;;--------------------------------------
 ;; Training API
 
@@ -73,14 +68,12 @@
         (update-in [:predictor :pred] train-pred this hst :taken)
         (update-in [:predictor :jump-map] assoc this next))))
 
-
 (defn train-step
   [processor]
   (let [{:keys [hst]} (:predictor processor)
         this          (common/register->val processor 31)]
     (-> processor
         (update-in [:predictor :pred] train-pred this hst :not-taken))))
-
 
 ;;--------------------------------------
 ;; History API
@@ -92,16 +85,13 @@
                (into (ring-buffer 9)
                      (conj hist outcome)))))
 
-
 (defn update-taken
   [processor]
   (update-history processor true))
 
-
 (defn update-not-taken
   [processor]
   (update-history processor false))
-
 
 ;;------------------------------------------------------------------------------
 ;; Implement the bits of the processor that have to change
@@ -123,7 +113,6 @@
                          :npc   npc
                          :pc    (+ pc 4)})))))
 
-
 (defn writeback
   "Pulls a writeback directive out of the processor state, and
   performs the indicated update on the processor state. Update command
@@ -135,55 +124,54 @@
         {:keys [dst addr val pc npc]} directive]
     (info "[writeback]" directive)
     (cond ;; special case to stop the show
-          (= :halt dst)
-            (assoc processor :halted true)
+      (= :halt dst)
+      (assoc processor :halted true)
 
-          ;; special case for hex code printing
-          (and (= :registers dst)
-               (= 29 addr))
-            (do (when-not (zero? val)
-                  (print (format "0x%X" (char val))))
-                processor)
+      ;; special case for hex code printing
+      (and (= :registers dst)
+           (= 29 addr))
+      (do (when-not (zero? val)
+            (print (format "0x%X" (char val))))
+          processor)
 
-          ;; special case for printing
-          (and (= :registers dst)
-               (= 30 addr))
-            (do (when-not (zero? val)
-                  (print (char val)))
-                processor)
+      ;; special case for printing
+      (and (= :registers dst)
+           (= 30 addr))
+      (do (when-not (zero? val)
+            (print (char val)))
+          processor)
 
-          ;; special case for branching as we must flush the pipeline
-          (and (= :registers dst)
-               (= 31 addr)
-               (not (= val npc))) ;; don't flush if we aren't changing
-                                  ;; the next PC value. This means to
-                                  ;; jumping to PC+4 does exactly
-                                  ;; nothing as it should.
-            (do (warn "[writeback] Branch mispredict!")
-                (warn "[writeback] flushing pipeline!")
-                (-> processor
-                    (update-taken)
-                    (train-jump (- pc 4) val)
-                    (dissoc :fetch)
-                    (dissoc :decode)
-                    (dissoc :execute)
-                    (assoc-in [:registers addr] val)))
+      ;; special case for branching as we must flush the pipeline
+      (and (= :registers dst)
+           (= 31 addr)
+           (not (= val npc))) ;; don't flush if we aren't changing
+      ;; the next PC value. This means to
+      ;; jumping to PC+4 does exactly
+      ;; nothing as it should.
+      (do (warn "[writeback] Branch mispredict!")
+          (warn "[writeback] flushing pipeline!")
+          (-> processor
+              (update-taken)
+              (train-jump (- pc 4) val)
+              (dissoc :fetch)
+              (dissoc :decode)
+              (dissoc :execute)
+              (assoc-in [:registers addr] val)))
 
-          ;; special case for correctly predicted branching
-          (and (= :registers dst)
-               (= 31 addr)
-               (= val npc)) ;; In this case we need to reinforce the
-                            ;; branch prediction.
-            (do  (info "[writeback] Branch predicted!")
-                 (-> processor
-                     (update-taken)
-                     (train-jump (- pc 4) val)))
+      ;; special case for correctly predicted branching
+      (and (= :registers dst)
+           (= 31 addr)
+           (= val npc)) ;; In this case we need to reinforce the
+      ;; branch prediction.
+      (do  (info "[writeback] Branch predicted!")
+           (-> processor
+               (update-taken)
+               (train-jump (- pc 4) val)))
 
-          true
-            (-> processor
-                (update-not-taken)
-                (assoc-in [dst addr] val)))))
-
+      true
+      (-> processor
+          (update-not-taken)
+          (assoc-in [dst addr] val)))))
 
 (defn step
   "Sequentially applies each phase of execution to a single processor
@@ -200,7 +188,6 @@
       p/decode
       fetch
       p/stall-dec))
-
 
 (defn -main
   "Steps a processor state until such time as it becomes marked as
